33533d691a5fbb337728a9b519ef22ce187e86c3
Edison Cuenca <ingfcuenca@gmail.com>
2025-07-23 12:22:32 -0400

    adding bullets

--- components/GlobalFooter.vue
+++ components/GlobalFooter.vue
@@ -1,6 +1,6 @@
 <template>
     <v-footer color="surface-light" name="footer">
-      <h1>Footer.</h1>
+      <h1>Footer</h1>
       <v-btn
         class="mx-auto"
         text="Get data"
@@ -9,3 +9,12 @@
       ></v-btn>
     </v-footer>
 </template>
+
+<script setup>
+  import { ref } from 'vue'
+  const app = ref()
+  const key = ref(123)
+  function print (key) {
+    alert(JSON.stringify(app.value.getLayoutItem(key), null, 2))
+  }
+</script>
--- components/GlobalNav.vue
+++ components/GlobalNav.vue
@@ -6,12 +6,18 @@
     </v-app-bar-nav-icon>
   </template>
   <v-app-bar-title>Forces.ca Style Guide</v-app-bar-title>
+    <div>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+        </ul>
+      </div>
   <template v-slot:append>
-    
       <v-btn prepend-icon="mdi-home"><NuxtLink to="/">Home</NuxtLink></v-btn>
       <v-btn prepend-icon="mdi-text-shadow"><NuxtLink to="/type">Type</NuxtLink></v-btn>
       <v-btn icon="mdi-magnify"></v-btn>
-
     <v-btn icon="mdi-dots-vertical"></v-btn>
   </template>
 </v-app-bar>
